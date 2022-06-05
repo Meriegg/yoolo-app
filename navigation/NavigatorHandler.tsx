2be258624105a6721470da9f7b0a63a48f06bb8b
Meriegg <ya.boybeats1234@gmail.com>
2022-06-05 23:23:11 +0300

    Added navigation

---
+++ navigation/NavigatorHandler.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+const NavigatorHandler = () => {
+  return <NavigationContainer>{}</NavigationContainer>;
+};
+
+export default NavigatorHandler;
---
+++ navigation/auth/AuthStackNavigator.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AuthStackNavigator = () => {};
+
+export default AuthStackNavigator;
